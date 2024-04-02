ef7d33ebd743558d6963b09a2672f39fa0154644
Jacob Samuel G. <26469677+tojacob@users.noreply.github.com>
2024-04-01 22:11:25 -0600

    fix bun route

--- src/bunjs/index.ts
+++ src/bunjs/index.ts
@@ -3,10 +3,14 @@ import { Elysia } from 'elysia';
 const app = new Elysia();
 const port = 80;
 
-app.post('/test/json', () => {
+app.post('/test/json', (ctx) => {
+  ctx.set.status = 418;
+  ctx.set.headers['x-powered-by'] = 'Elysia';
+
   return { success: true };
 });
 
 app.listen(port, () => {
+
   console.log(`Elysia: server listening in port '${port}'`);
 });
